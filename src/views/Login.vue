fa63f15af1a13aa62d77c7df50243bb91678f88e
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-26 14:48:55 -0600

    got modal working for login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="login">
+    <!-- header for logo, need to fix style -->
     <header>
       <img src="../assets/pupLogo.png" class="rounded float-left">
-
     </header>
-    <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 
+    <!-- modal time -->
+    <!-- Large modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">LOGIN</button>
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address:</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password:</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+
+        </div>
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-
+    </div>
   </div>
 </template>
